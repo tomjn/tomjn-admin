e841dd5fb17e83d309f27d2053b83e7c5891f661
Tarendai <contact@tomjn.com>
2015-06-06 17:05:01 +0100

    adjust header to include hooks and the top level menus

--- php/MenuHeader.php
+++ php/MenuHeader.php
@@ -21,6 +21,7 @@ class MenuHeader {
 	public function run() {
 		add_action( 'adminmenu', array( $this, 'adminmenu' ), PHP_INT_MAX );
 		add_action( 'in_admin_header', array( $this, 'in_admin_header' ), PHP_INT_MAX + 1 );
+		add_action( 'wp_dashboard_setup', array( $this, 'wp_dashboard_setup' ) );
 	}
 
 	public function adminmenu() {
@@ -35,9 +36,35 @@ class MenuHeader {
 			</div>
 		</div>
 		<div id="tomjnadminhead">
-			<h2><?php bloginfo('name');?></h2>
+			<?php
+			global $menu, $submenu, $parent_file;
+			foreach ( $menu as $key => $item ) {
+				if ( !current_user_can( $item[1] ) ) {
+					continue;
+				}
+				$title = $item[0];
+				if ( empty( $title ) ) {
+					continue;
+				}
+				$title = wptexturize( $item[0] );
+				echo '<a href="'.admin_url( $item[2]).'">'.$title.'</a> ';
+			}
+			?>
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+			<?php do_action('tomjn_header_end'); ?>
 		</div>
 		<div id="wpcontent">
 		<?php
+	}
+
+	public function wp_dashboard_setup() {
+		add_action( 'tomjn_header_end', array( $this, 'dash_tomjn_header_end' ) );
+	}
+
+	public function dash_tomjn_header_end() {
+		global $menu, $submenu, $parent_file;
+		echo '<pre>';
+		print_r( $menu );
+		echo '</pre>';
 	}
 }
